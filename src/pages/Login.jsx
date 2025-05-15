630068c1ca052faad4a46b3ffa6d972de23b70f2
DmytroCherednichenko <creator1517@gmail.com>
2025-05-15 04:27:16 -0300

    add login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { Container, Button, Form } from "react-bootstrap";
+import "./login.css"
 
 const Login = () => {
     const navigate = useNavigate();
@@ -10,9 +11,25 @@ const Login = () => {
     };
 
     return (
-        <div>
-            <Button onClick={()=>handleLogin()}>LOL</Button>
-        </div>
+        <Container className="login">
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control className="form-field" type="email" placeholder="Enter email" />
+                    <Form.Text className="text-muted">
+                        We'll never share your email with anyone else.
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control className="form-field" type="password" placeholder="Password" />
+                </Form.Group>
+                <Button variant="primary" onClick={()=>handleLogin()}>
+                    Submit
+                </Button>
+            </Form>
+        </Container>
     )
 }
 
